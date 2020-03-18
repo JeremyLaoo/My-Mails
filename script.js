8f3be5af05f5b5b5716cae9f9adf1aa6e4f4ed01
Jérémy Lao <lao.jeremy@live.fr>
2020-03-18 16:26:20 +0100

    JavaScript Dom Click ok and delete with trash

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ console.log('messagesCount :', messagesCount);
 // Le .textContent permet de modifier le texte sur lequel on accede ici "count"
 document.getElementById('count').textContent = messagesCount;
 
+for(var i=0; i<document.getElementsByClassName('trash').length; i++){
+// Mécanique qui permet de capter le click sur le picto 'trash'
+document.getElementsByClassName('trash')[i].addEventListener("click",
+ function(){
+  console.log("clic détecté !");
+  this.parentNode.remove()
+  var messagesCount = document.getElementsByTagName('p').length
+  document.getElementById('count').textContent = messagesCount;
+ }
+);
+}
