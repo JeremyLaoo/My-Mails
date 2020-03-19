d56d7a9df0ad0653c8f2155f56fafc9684f8b694
Jérémy Lao <lao.jeremy@live.fr>
2020-03-19 14:09:51 +0100

    Add newDiv

--- script.js
+++ script.js
@@ -18,3 +18,60 @@ document.getElementsByClassName('trash')[i].addEventListener("click",
  }
 );
 }
+// On capte le Click, on supprime l'élément parent de la cible, ici la poubelle donc parent = la div et on recompte dans la fonction pour mettre à jour le compteur
+
+document.getElementById('button').addEventListener("click",
+ function(){
+  console.log("clic détecté !");
+//   .value recupère la valeur du champs input
+  var newMessage = document.getElementById('input').value
+  console.log('newMessage :', newMessage);
+
+  var newDiv = document.createElement('div');
+  newDiv.className = "profil";
+  document.body.appendChild(newDiv);
+
+  var image = document.createElement('img');
+  image.className = "avatar";
+  image.src = "avatar-1.jpg";
+  newDiv.appendChild(image);
+
+  var nameMessageDiv = document.createElement('div');
+  nameMessageDiv.className = "body";
+  newDiv.appendChild(nameMessageDiv);
+
+  var title = document.createElement('h4');
+  title.className = "title";
+  title.textContent = newMessage ;  
+  nameMessageDiv.appendChild(title);
+
+  var desc = document.createElement('p');
+  desc.textContent = "Ici c'est Paris";
+  nameMessageDiv.appendChild(desc);
+
+  var trashPicto = document.createElement('img');
+  trashPicto.className = "trash";
+  trashPicto.src = "trash.png";
+  newDiv.appendChild(trashPicto);
+
+  var messagesCount = document.getElementsByTagName('p').length
+  document.getElementById('count').textContent = messagesCount;
+
+  document.getElementById('input').value = "";
+
+  trashPicto.addEventListener('click', 
+  function(){
+        console.log("click ok")
+        this.parentNode.remove();
+        var messagesCount = document.getElementsByTagName('p').length
+        document.getElementById('count').textContent = messagesCount;
+  })
+  
+ }
+ );
+
+//  document.getElementsByClassName('trash').addEventListener("click", 
+//   function(){
+//       console.log("clic ok");
+//   })
+
