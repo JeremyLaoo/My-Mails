5967733eac41b2dddb7309a35a29156353922ee1
Jérémy Lao <lao.jeremy@live.fr>
2020-03-19 16:50:22 +0100

    Search Bar

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $('#count').text(messagesCount);
 // Mécanique qui permet de capter le click sur le picto 'trash'
 $('.trash').click(
  function(){
-  console.log("clic détecté !");
+  console.log("Click first trash");
   this.parentNode.remove()
   var messagesCount = $('p').length
   $('#count').text(messagesCount);
@@ -20,48 +20,69 @@ $('.trash').click(
 // }
 // On capte le Click, on supprime l'élément parent de la cible, ici la poubelle donc parent = la div et on recompte dans la fonction pour mettre à jour le compteur
 
-$('#button').click(
+$('.button').click(
  function(){
   console.log("clic détecté !");
 //   .value recupère la valeur du champs input
-  var newMessage = $('#input').value
-  console.log('newMessage :', newMessage);
+  var newMessage = $('.input').val();
+  console.log('Message from Add :', newMessage);
+  
+// Ecriture JQuery pour simplifier JS 
+$("body").append(`
+<div class="profil">
+  <img class="avatar" src="avatar-1.jpg">
+  <div class="body">
+    <h4 class="title"> John Doe </h4>
+    <p> `+newMessage+`</p>
+  </div>
+  <img class="trash" src="trash.png">
+</div>
+`)
 
-  var newDiv = document.createElement('div');
-  newDiv.className = "profil";
-  document.body.appendChild(newDiv);
 
-  var image = document.createElement('img');
-  image.className = "avatar";
-  image.src = "avatar-1.jpg";
-  newDiv.appendChild(image);
 
-  var nameMessageDiv = document.createElement('div');
-  nameMessageDiv.className = "body";
-  newDiv.appendChild(nameMessageDiv);
 
-  var title = document.createElement('h4');
-  title.className = "title";
-  title.textContent = newMessage ;  
-  nameMessageDiv.appendChild(title);
+// Ecriture JavaScript Standard 
 
-  var desc = document.createElement('p');
-  desc.textContent = "Ici c'est Paris";
-  nameMessageDiv.appendChild(desc);
+//   var newDiv = document.createElement('div');
+//   newDiv.className = "profil";
+//   document.body.appendChild(newDiv);
 
-  var trashPicto = document.createElement('img');
-  trashPicto.className = "trash";
-  trashPicto.src = "trash.png";
-  newDiv.appendChild(trashPicto);
+//   var image = document.createElement('img');
+//   image.className = "avatar";
+//   image.src = "avatar-1.jpg";
+//   newDiv.appendChild(image);
+
+//   var nameMessageDiv = document.createElement('div');
+//   nameMessageDiv.className = "body";
+//   newDiv.appendChild(nameMessageDiv);
+
+//   var title = document.createElement('h4');
+//   title.className = "title";
+//   title.textContent = newMessage ;  
+//   nameMessageDiv.appendChild(title);
+
+//   var desc = document.createElement('p');
+//   desc.textContent = "Ici c'est Paris";
+//   nameMessageDiv.appendChild(desc);
+
+//   var trashPicto = document.createElement('img');
+//   trashPicto.className = "trash";
+//   trashPicto.src = "trash.png";
+//   newDiv.appendChild(trashPicto);
+
+// Fin écriture JS standard 
 
   var messagesCount = document.getElementsByTagName('p').length
   $('#count').text(messagesCount);
 
-  document.getElementById('input').value = "";
+  
 
-  trashPicto.addEventListener('click', 
+  $('.input').val();
+
+  $('.trash').click(
   function(){
-        console.log("click ok")
+        console.log("click remove trash2")
         this.parentNode.remove();
         var messagesCount = document.getElementsByTagName('p').length
         $('#count').text(messagesCount);
@@ -70,8 +91,24 @@ $('#button').click(
  }
  );
 
-//  document.getElementsByClassName('trash').addEventListener("click", 
-//   function(){
-//       console.log("clic ok");
-//   })
+ $('.input').text("");
 
+//  Ecoute du bon bouton search 
+
+$('#search').click(
+  function(){
+      console.log("click search OKK")
+      var searchMsg = $('#searchMsg').val();
+      console.log('Message from Search :', searchMsg);
+
+      $('h4').each(function(){
+        if($("#searchMsg").val() != $(this).text() ){  
+          $(this).parent().parent().fadeOut();
+       } else {
+        $(this).parent().parent().show();
+       }
+     })
+
+  
+        
+  })
